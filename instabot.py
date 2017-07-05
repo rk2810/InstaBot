aefefe0fc58157b16b4c53dbad648fda3d60a367
Rohan Khurana <rohan.kumquat@gmail.com>
2017-07-06 03:18:32 +0530

    Implementation: retrieve self info

--- instabot.py
+++ instabot.py
@@ -1,6 +1,7 @@
 # imports
 import requests as req
 import json
+import urllib
 from access_token import *  # Import Access token
 
 # general variables
@@ -21,18 +22,36 @@ def main():
     while ans:
         print """
                 1.Search by user 
-                2.xyyz
+                2.Get your info
                 3.Exit
                 """
         ans = raw_input("What would you like to do ? ")
         if ans == "1":
             search_user()
         elif ans == "2":
-            exit()
+            self()
         elif ans == "3":
             exit()
         elif ans != "":
             print "Invalid choice... try again!"
+
+
+def self():
+    print "Fetching your last image : "
+    request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % ACCESS_TOKEN
+    print 'GET request url : %s' % request_url
+    own_media = req.get(request_url).json()
+
+    if own_media['meta']['code'] == 200:
+        if len(own_media['data']):
+            image_name = own_media['data'][0]['id'] + '.jpeg'
+            image_url = own_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Fetch successful!'
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
 
 
 def search_user():
